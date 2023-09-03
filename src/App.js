1e9327404d1a867b8e4ff2bb6a1e310a2a1b00af
Brandon Spear <Brandon.Spear97@gmail.com>
2023-09-03 09:26:10 -0700

    readd package.json

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SignUp from './components/auth/SignUp';
-import SignIn from './components/auth/SignIn';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/navigation/Navbar';
+import SelectAccount from './path_to_SelectAccount_Component'; // Adjust the import path
+import Login from './path_to_Login_Component'; // Adjust the import path
 
-function App() {
+const App = () => {
     return (
         <Router>
+            <Navbar />
             <Switch>
-                <Route path="/signup" component={SignUp} />
-                <Route path="/signin" component={SignIn} />
-                {}
+                <Route path="/select-account">
+                    <SelectAccount />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                {/* Add other routes as necessary */}
             </Switch>
         </Router>
     );
