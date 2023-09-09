e31d0984722029ffeaaf77e69a78a4fbe4a68dbe
Brandon Spear <Brandon.Spear97@gmail.com>
2023-09-08 17:44:37 -0700

    Successfully hide home button on login screen and home page.

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 
 function Navbar() {
   const { currentUser, setCurrentUser } = useContext(AuthContext);
   const navigate = useNavigate();
+  const location = useLocation();
 
   console.log(currentUser);
 
@@ -15,7 +16,7 @@ function Navbar() {
 
   return (
     <nav>
-      {currentUser && <Link to="/">Home</Link>}
+      {currentUser && location.pathname !== '/home' && <Link to="/">Home</Link>}
       {!currentUser ? (
         <>
           <Link to="/sign-up">Sign Up</Link>
@@ -29,6 +30,11 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+
+
+
 
 
 
