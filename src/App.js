85b00b333fd5b84f66551efb9333c337833e4cf4
Brandon Spear <Brandon.Spear97@gmail.com>
2023-09-25 22:40:52 -0700

    update project.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Navbar from './components/navigation/Navbar';
-// import SignIn from './components/auth/SignIn';
-// import SignUp from './components/auth/SignUp';
-// import Home from './components/navigation/Home';
-// import { AuthProvider } from './context/AuthContext';
-
-// const App = () => {
-//     console.log("app is rendering");
-//     return (
-//         <AuthProvider>
-//             <Router>
-//                 <h1>Chore Chracker</h1>
-//              <Navbar />
-//                 <Routes>
-//                     <Route path="/sign-in" element={<SignIn />} />
-//                     <Route path="/sign-up" element={<SignUp />} />
-//                     <Route path="/home" element={<Home />} />
-//                 </Routes>
-//             </Router>
-//         </AuthProvider>
-//     );
-// }
-
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
@@ -65,5 +36,4 @@ const App = () => {
 };
 
 export default App;
-
 
--- src/components/navigation/Home.js
+++ src/components/navigation/Home.js
@@ -21,7 +21,7 @@ function Home() {
 
   return (
     <div>
-      <h1>Home</h1>
+      <h2>Home</h2>
       <ParentProfile />
       {kids.map(kid => (
         <div 
